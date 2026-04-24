aa1d8225a2b34682849d251da4dc346599b9d205
Monike <monikeozsantos06@gmail.com>
2026-04-24 16:47:20 +0100

    Improved analysis w top companies

--- src/pipeline/analyzer.py
+++ src/pipeline/analyzer.py
@@ -1,10 +1,11 @@
+import pandas as pd
+
 def analyze(df, top_n=20):
     # Analyzes dataset n return insights
     print("Running analyze...")
 
-    results = {}
+    # Top companies hiring
+    result_df = df["company_name"].value_counts().head(top_n).reset_index()
+    result_df.columns = ["company_name", "job_count"]
 
-    # example
-    results["row_count"] = len(df)
-
-    return results
+    return result_df
