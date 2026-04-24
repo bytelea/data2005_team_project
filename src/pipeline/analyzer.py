d7d5ab4c0dce5ec2c6d894ab594632d9a8d6735b
Monike <monikeozsantos06@gmail.com>
2026-04-24 17:05:01 +0100

    Clean commit without dataset + added features and analysis

--- src/pipeline/analyzer.py
+++ src/pipeline/analyzer.py
@@ -1,11 +1,24 @@
 import pandas as pd
 
 def analyze(df, top_n=20):
-    # Analyzes dataset n return insights
+    # Analyzes dataset and return insights
     print("Running analyze...")
+    
+    results = {}
 
     # Top companies hiring
     result_df = df["company_name"].value_counts().head(top_n).reset_index()
     result_df.columns = ["company_name", "job_count"]
 
+    # Top companies 
+    top_companies = (
+        df["company_name"]
+        .value_counts()
+        .head(top_n)
+        .reset_index()
+    )
+    top_companies.columns = ["company_name", "job_count"]
+    results["top_companies"] = top_companies
+
+    
     return result_df
--- src/pipeline/features.py
+++ src/pipeline/features.py
@@ -1,5 +1,17 @@
+import pandas as pd
+
 def extract_features(df):
-    # Extracts useful features from dataset
+    # Extracts useful features from job descriptions
     print("Running extract_features...")
+
+    # Make sure description exists
+    df["description"] = df["description"].fillna("").str.lower()
+
+    # List of skills to detect
+    skills = ["python", "sql", "excel", "tableau", "power bi"]
+
+    # Create new columns for each skill
+    for skill in skills:
+        df[skill] = df["description"].str.contains(skill, na=False)
 
     return df
